0438f841fb51d20937c3d6bf8aaa14163d19c43d
robwiss <robwiss@gmail.com>
2013-10-04 15:49:37 -0400

    added colorblind friendly colors

--- pygame_colors.py
+++ pygame_colors.py
@@ -2,17 +2,19 @@ import engine_color as engine
 import pygame
 from pygame.gfxdraw import pixel as p
 import itertools
+import time
 
 def main():
-    scale = 4
+    scale = 5
+    sleep_interval = 0
 
     x, y = 200, 200
     colors = [
-        (113, 255, 53),
-        (232, 176, 36),
-        (255, 65,  53),
-        (101, 36,  232),
-        (40,  246, 255),
+        (0,   14,  255),
+        (12,  232, 146),
+        (250, 255, 0),
+        (232, 115, 12),
+        (255, 13,  226),
     ]
 
     cells = engine.randomized_cells((x,y), colors)
@@ -20,7 +22,7 @@ def main():
 
     def process_living(cells):
         for (x1, y1) in cells:
-            for x_add, y_add in itertools.permutations(xrange(4), 2):
+            for x_add, y_add in itertools.permutations(xrange(scale), 2):
                 p(screen, x1 * scale + x_add, y1 * scale + y_add, cells.get_color((x1,y1)))
 
     pygame.init()
@@ -32,6 +34,7 @@ def main():
         pygame.display.get_surface().fill(gray10)
         process_living(cells)
         engine.tick(cells)
+        time.sleep(sleep_interval)
  
 if __name__ == "__main__":
     main()
